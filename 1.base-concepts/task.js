95f8e36d2c777b8b8368067d0a9d5e6e514f355b
Ilya Duzhakov <duzhakovip@mail.ru>
2024-12-27 13:48:22 +0300

    Реализовано первое и второе задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
-"use strict"
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let d = Math.pow(b, 2) - 4 * a * c
+	if (d < 0) {
+		return []
+	} else if (d === 0) {
+		let result = -b / (2 * a)
+		return [result]
+	} else if (d > 0) {
+		let result1 = (-b + Math.sqrt(d)) / (2 * a)
+		let result2 = (-b - Math.sqrt(d)) / (2 * a)
+		return [result1, result2]
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let P = (percent / 100) / 12
+	let S = amount - contribution
+	let count = S * (P + (P / (((1 + P) ** countMonths) - 1)))
+	let result = (count * countMonths).toFixed(2)
+	console.log(typeof Number(result))
+	return (+result)
 }
